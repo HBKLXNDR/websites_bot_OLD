3b94cec8ff5d548ad69055eececec1c52126f611
HBKLXNDR <hbklksndr@protonmail.com>
2024-08-06 01:00:57 +0300

    added vercel config

--- api/bot.js
+++ api/bot.js
@@ -2,15 +2,14 @@ const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
 const cors = require('cors');
 
-// const token = process.env.BOT_TOKEN;
-const token = '6864665229:AAEncCx8DSCa9mb918qTI-c37mlFw47Oz-I'
-const webAppUrl = 'https://66ae3293fe7aad955f2182ca--magenta-naiad-786a93.netlify.app/';
-
-const bot = new TelegramBot(token, {polling: true});
 const app = express();
-
 app.use(express.json());
 app.use(cors());
+
+// Initialize bot
+const token = process.env.BOT_TOKEN || '6864665229:AAEncCx8DSCa9mb918qTI-c37mlFw47Oz-I';
+const webAppUrl = process.env.WEB_APP_URL || 'https://66ae3293fe7aad955f2182ca--magenta-naiad-786a93.netlify.app/';
+const bot = new TelegramBot(token, { polling: true });
 
 bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
@@ -20,21 +19,21 @@ bot.on('message', async (msg) => {
         await bot.sendMessage(chatId, 'Заходьте на наш сайт!', {
             reply_markup: {
                 keyboard: [
-                    [{text: 'Замовити сайт', web_app: {url: webAppUrl}}, {text: 'Залишити заявку', web_app: {url: webAppUrl + 'form'}}]
+                    [{ text: 'Замовити сайт', web_app: { url: webAppUrl } }, { text: 'Залишити заявку', web_app: { url: webAppUrl + 'form' } }]
                 ]
             }
-        })
+        });
     }
 
     if (msg?.web_app_data?.data) {
         try {
-            const data = JSON.parse(msg?.web_app_data?.data)
-            console.log(data)
-            await bot.sendMessage(chatId, "Дякую за зворотній зв'язок!")
+            const data = JSON.parse(msg?.web_app_data?.data);
+            console.log(data);
+            await bot.sendMessage(chatId, "Дякую за зворотній зв'язок!");
 
             setTimeout(async () => {
                 await bot.sendMessage(chatId, 'Всю інформацію Ви отримаєте у цьому чаті');
-            }, 3000)
+            }, 3000);
         } catch (e) {
             console.log(e);
         }
@@ -42,23 +41,20 @@ bot.on('message', async (msg) => {
 });
 
 app.post('/web-data', async (req, res) => {
-    const {queryId, products = [], totalPrice} = req.body;
+    const { queryId, products = [], totalPrice } = req.body;
     try {
         await bot.answerWebAppQuery(queryId, {
             type: 'article',
             id: queryId,
             title: 'Успішна купівля',
             input_message_content: {
-                message_text: ` Вітаю зі зверненням, вы купили товар на суму ${totalPrice},
-                ${products.map(item => item.title).join(', ')}`
+                message_text: ` Вітаю зі зверненням, вы купили товар на суму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
             }
-        })
+        });
         return res.status(200).json({});
     } catch (e) {
-        return res.status(500).json({})
+        return res.status(500).json({});
     }
-})
+});
 
-const PORT = 8000;
-
-app.listen(PORT, () => console.log('server started on PORT ' + PORT))
+module.exports = app;
