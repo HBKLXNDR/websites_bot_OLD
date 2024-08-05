16b32f2d693f4e7abb56b248e5c4f109cb2459be
HBKLXNDR <hbklksndr@protonmail.com>
2024-08-06 02:21:20 +0300

    TG bot: added text and transfering the message to the needed account

--- api/bot.js
+++ api/bot.js
@@ -6,12 +6,14 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: 'https://66ae3293fe7aad955f2182ca--magenta-naiad-786a93.netlify.app/'
+    origin: process.env.WEB_APP_URL
 }));
+
+const url = process.env.HOMEPAGE_URL;
 
 // Initialize bot
 const token = process.env.BOT_TOKEN || '';
-const webAppUrl = process.env.WEB_APP_URL || 'https://66ae3293fe7aad955f2182ca--magenta-naiad-786a93.netlify.app/';
+const webAppUrl = process.env.WEB_APP_URL || '';
 const bot = new TelegramBot(token, { polling: true });
 
 bot.on('message', async (msg) => {
@@ -22,7 +24,9 @@ bot.on('message', async (msg) => {
         await bot.sendMessage(chatId, 'Заходьте на наш сайт!', {
             reply_markup: {
                 keyboard: [
-                    [{ text: 'Замовити сайт', web_app: { url: webAppUrl } }, { text: 'Залишити заявку', web_app: { url: webAppUrl + 'form' } }]
+                    [
+                        { text: 'Замовити сайт', web_app: { url: webAppUrl } },{ text: 'Залишити заявку', web_app: { url: webAppUrl + 'form' } }
+                    ]
                 ]
             }
         });
@@ -31,11 +35,16 @@ bot.on('message', async (msg) => {
     if (msg?.web_app_data?.data) {
         try {
             const data = JSON.parse(msg?.web_app_data?.data);
-            console.log(data);
-            await bot.sendMessage(chatId, "Дякую за зворотній зв'язок!");
+            console.log(data, chatId);
+            await bot.sendMessage(chatId, `Дякую за зворотній зв'язок!, Ваш chatId: ${chatId}`);
+            await bot.sendMessage(process.env.TG_ID, `Нова заявка: ${data.email}, ${data.number}, ${data.name}`)
 
             setTimeout(async () => {
-                await bot.sendMessage(chatId, 'Всю інформацію Ви отримаєте у цьому чаті');
+                await bot.sendMessage(chatId, `
+                Всю інформацію Ви отримаєте у цьому чаті: @financial_grammarly,
+                а поки наш менеджер займається обробкою Вашої заявки,
+                завітайте на наш сайт! ${url}
+                `);
             }, 3000);
         } catch (e) {
             console.log(e);
